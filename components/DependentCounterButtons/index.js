5595ddaeb71cef602d486e85098a0a94feb10eb1
Sebastian <ensuramerica@gmail.com>
2023-08-01 12:12:51 -0500

    useState Counters examples

---
+++ components/DependentCounterButtons/index.js
@@ -0,0 +1,10 @@
+import CounterButtonsGroup from "../CounterButtonsGroup"
+
+export default function DependentCounterButtons() {
+    return (
+        <section>
+            <h2>Dependent counter buttons</h2>
+            <CounterButtonsGroup />
+        </section>
+    )
+}
